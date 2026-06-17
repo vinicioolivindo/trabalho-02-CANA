99ce699d629726d86b62fe9d803e3d7443a7b6f9
v1nny:) <vinicioolivindo81@gmail.com>
2026-06-17 19:37:46 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,37 @@
+ # Trabalho Computacional 02 - Construção e Análise de Algoritmos 2025.1
+ ## Instituto Federal de Educação, Ciência e Tecnologia do Ceará (IFCE) - Campus Tianguá
+ ## Bacharelado em Ciência da Computação
+ ## Equipe X
+
+<p align="center">
+Este repositório contém as soluções do Trabalho Computacional 02 da disciplina de Construção e Análise de Algoritmos 2025.1. 
+</p>
+
+
+<div align="justify">
+
+## 🤖 Membros da equipe
+
+* Nome completo 1
+* Nome completo 2
+* Nome completo 3
+* Nome completo 4
+* Nome completo 5
+
+ ## 📋 Requisitos ou Instruções
+
+* Insira uma lista de requisitos de software para usar executar seus programas ou orientações que considere pertinente
+
+
+## 👏 Contato
+
+Se houver um erro ou outra melhoria que você gostaria de relatar ou solicitar, incentivamos você a contribuir.
+
+Por favor, sinta-se à vontade para entrar em contato conosco para qualquer dúvida: [![Gmail Badge](https://img.shields.io/badge/-email@ifce.edu.br-c14438?style=flat-square&logo=Gmail&logoColor=white&link=mailto:email@ifce.edu.br)](mailto:email@ifce.edu.br)
+
+
+
+## 📚 Referências
+
+* Lista 1
+* <a href="https://www.sciencedirect.com/science/article/pii/S1877050922009668"><strong>Exemplo 02</strong></a>.
---
+++ qst24-Jose-Vinicio.c
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct No {
+    int valor;
+    struct No* ant;
+    struct No* prox;
+} No;
+
+void inserir(No** inicio, int valor) {
+
+    No* novo = malloc(sizeof(No));
+
+    novo->valor = valor;
+    novo->prox = NULL;
+    novo->ant = NULL;
+
+    if (*inicio == NULL) {
+        *inicio = novo;
+        return;
+    }
+
+    No* atual = *inicio;
+
+    while (atual->prox)
+        atual = atual->prox;
+
+    atual->prox = novo;
+    novo->ant = atual;
+}
+
+No* buscaSequencial(No* inicio, int chave) {
+
+    while (inicio != NULL) {
+
+        if (inicio->valor == chave)
+            return inicio;
+
+        inicio = inicio->prox;
+    }
+
+    return NULL;
+}
+
+No* encontrarMeio(No* inicio, No* fim) {
+
+    No* lento = inicio;
+    No* rapido = inicio;
+
+    while (
+        rapido != fim &&
+        rapido->prox != fim
+    ) {
+
+        rapido = rapido->prox;
+
+        if (rapido != fim) {
+            rapido = rapido->prox;
+            lento = lento->prox;
+        }
+    }
+
+    return lento;
+}
+
+No* buscaBinaria(No* inicio, int chave) {
+
+    No* esquerda = inicio;
+    No* direita = NULL;
+
+    while (esquerda != direita) {
+
+        No* meio = encontrarMeio(esquerda, direita);
+
+        if (meio == NULL)
+            return NULL;
+
+        if (meio->valor == chave)
+            return meio;
+
+        if (chave > meio->valor)
+            esquerda = meio->prox;
+        else
+            direita = meio;
+    }
+
+    return NULL;
+}
+
+int main() {
+
+    No* lista = NULL;
+
+    inserir(&lista, 10);
+    inserir(&lista, 20);
+    inserir(&lista, 30);
+    inserir(&lista, 40);
+    inserir(&lista, 50);
+
+    int valor = 40;
+
+    No* resultado;
+
+    resultado = buscaSequencial(lista, valor);
+
+    if (resultado)
+        printf("Busca sequencial encontrou: %d\n", resultado->valor);
+    else
+        printf("Nao encontrado\n");
+
+    resultado = buscaBinaria(lista, valor);
+
+    if (resultado)
+        printf("Busca binaria encontrou: %d\n", resultado->valor);
+    else
+        printf("Nao encontrado\n");
+
+    return 0;
+}
